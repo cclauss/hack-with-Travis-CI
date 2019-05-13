21fb61f72407edaead600cced7c749fc540141fb
cclauss <cclauss@me.com>
2019-05-14 01:34:43 +0200

    print(f"export PATH={Path(sys.executable).parent.as_posix().replace('C:', '/c')}:$PATH")

--- make_path_sh.py
+++ make_path_sh.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
-
 import sys
-
-before = "PATH environment variable does not have"
-after = r"\bin"
-app_path = (sys.stdin.read().partition(before)[2].split(after)[0] + after).strip()
-assert app_path != 'after'
-print(f"export PATH={app_path}:$PATH")
-#with open('temp.bash', 'w') as out_file:
-#    out_file.write(f"#!/usr/bin/env bash\nexport PATH={app_path}:$PATH")
+from pathlib import Path
+print(f"export PATH={Path(sys.executable).parent.as_posix().replace('C:', '/c')}:$PATH")
